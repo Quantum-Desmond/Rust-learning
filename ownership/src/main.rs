3e67427d9f90480cef28daddb9cbb6f2bfb1ebb9
Karl McCarron <karlmccarron@hotmail.com>
2017-08-12 17:04:08 +0100

    WIP: initial commit of ownership folder

---
+++ ownership/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+   println!("start of ownership chapter.");
+
+   // summary: when giving a variable a value,
+   // it depends if it's simple data or not (similar
+   // to POD in C++); move semantic for most, copy if POD
+
+   
+
+
+}
