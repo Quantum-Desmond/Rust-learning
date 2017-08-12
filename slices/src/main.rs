18e226ef25e6b0ef776aba3f61b05192cc284c97
Karl McCarron <karlmccarron@hotmail.com>
2017-08-12 18:16:27 +0100

    Added slices project

---
+++ slices/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+    	if item == b' ' {
+    		return &s[..i];
+    	}
+    }
+
+    &s[..]
+}
