93e826b10cad7f8f660056b67b683dbfc609a00d
Karl McCarron <karlmccarron@hotmail.com>
2017-08-18 16:05:44 +0100

    Added errors and generics projects

---
+++ errors/target/debug/errors.d
@@ -0,0 +1,1 @@
+C:\Users\User\code\Rust-learning\errors\target\debug\errors.exe: C:\Users\User\code\Rust-learning\errors\src\main.rs
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+C:\Users\User\code\Rust-learning\generics\target\debug\generics.exe: C:\Users\User\code\Rust-learning\generics\src\main.rs
